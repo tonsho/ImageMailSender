2d071c30582dd17a782564bd28d7d0dfd1ff2900
Kazutaka Tonsho <Kazutaka.Tonsho@gmail.com>
2014-08-08 23:29:32 +0900

    New project

---
+++ src/com/tonton/imagemailsender/ImageMailSenderServlet.java
@@ -0,0 +1,13 @@
+package com.tonton.imagemailsender;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class ImageMailSenderServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
